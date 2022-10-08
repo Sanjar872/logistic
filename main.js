9cb469dc7bd2f577a64725ca47e2d5f77495f1c2
Sanjar872 <Sanjarbek.muxiddinov@gmail.com>
2022-10-08 09:43:27 +0300

    sanjarbek

---
+++ main.js
@@ -0,0 +1,43 @@
+// const langEN = document.querySelector('.langWrap');
+// const link = document.querySelectorAll('a');
+
+// const titleEl = document.querySelector(".title")
+// const description = document.querySelector(".description")
+
+// link.forEach(el => {
+//     el.addEventListener('click',()=> {
+//         langEN.querySelector('.active').classList.remove('active');
+//         el.classList.add('avtive');
+
+//         const attr = el.getAttribute('language');
+//         titleEl.textContent = data[attr].title;
+//         description.textContent = data[attr].description;
+//     })
+// })
+
+
+
+
+// var data = {
+//     "english":
+//         {
+//         "title" : "Hello Word",
+//         "description":
+//         "lorem ipsum doloor sut amet consectere sdahdne jda  ajsdnakjdsnhu asjdbajhsbduqwhda akjnsdjasndqw asdjnajsdn"
+
+//         },
+//         "russiya" : 
+//         {
+//             "title":"Здрасвуйте",
+//             "description" :
+//             "asidhqwuidbasldjabdjqwdnajsndnahsbda5155lsnd"
+//         },
+
+//         "uzbekcha":
+//         {
+//             "title": "salom dunyoo",
+//             "description" :
+//             "asdjaklsdjaknsdiasdnakjdnkajnsd sanjjjrrr sanjarrrrrr"
+//         }
+
+// }
